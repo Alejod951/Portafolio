1dc074987eb9c56f8cd142611441ef51ed2490f9
Alejod951 <Alejod951@gmail.com>
2025-01-05 16:24:30 -0500

    Proyectos y card second p

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -2,35 +2,36 @@ export const projects = [
   {
     id: 1,
     title: {
-      en: "Eye Tracking and Facial Key Points for Emotion Detection",
-      es: "Seguimiento de los Ojos y Puntos Clave de la Cara para la Detección de Emociones",
+      es: "Eye Tracking and Facial Key Points for Emotion Detection",
+      en: "Seguimiento de los Ojos y Puntos Clave de la Cara para la Detección de Emociones",
     },
     image: "https://github.com/Alejod951/Portafolio/blob/main/src/assets/FaceMesh.png?raw=true",
     shortDescription: {
-      en: "A project for detecting emotions, microsleeps, and gaze direction using Python, MediaPipe, Google libraries, and ROS.",
-      es: "Un proyecto para detectar emociones, microsueños y la dirección de la mirada usando Python, MediaPipe, bibliotecas de Google y ROS.",
+      es: "A project for detecting emotions, microsleeps, and gaze direction using Python, MediaPipe, Google libraries, and ROS.",
+      en: "Un proyecto para detectar emociones, microsueños y la dirección de la mirada usando Python, MediaPipe, bibliotecas de Google y ROS.",
     },
     longDescription: {
-      en: "This project leverages Python and the MediaPipe library from Google to analyze eye movements and facial key points. It determines emotions, microsleeps, and gaze direction, with applications in robotics perception using ROS.",
-      es: "Este proyecto utiliza Python y la biblioteca MediaPipe de Google para analizar movimientos oculares y puntos clave de la cara. Permite determinar emociones, microsueños y la dirección de la mirada, con aplicaciones en la percepción robótica utilizando ROS.",
+      es: "This project leverages Python and the MediaPipe library from Google to analyze eye movements and facial key points. It determines emotions, microsleeps, and gaze direction, with applications in robotics perception using ROS.",
+      en: "Este proyecto utiliza Python y la biblioteca MediaPipe de Google para analizar movimientos oculares y puntos clave de la cara. Permite determinar emociones, microsueños y la dirección de la mirada, con aplicaciones en la percepción robótica utilizando ROS.",
     },
     githubLink: "https://github.com/Alejod951/EyeTrackingEmotionDetectionBlinkCounterSleepMonitoringRepository",
   },
-    {
-      id: 2,
-      title: {
-        en: "Aplicación de Tareas",
-        es: "Task Management App",
-      },
-      image: "https://github.com/Alejod951/Portafolio/blob/main/src/assets/FaceMesh.png?raw=true",
-      shortDescription: {
-        en: "Una aplicación para gestionar tareas.",
-        es: "An application for managing tasks.",
-      },
-      longDescription: {
-        en: "Aplicación desarrollada con React y TypeScript que permite crear, editar y eliminar tareas. Incluye persistencia local y un diseño responsivo.",
-        es: "App developed with React and TypeScript that allows creating, editing, and deleting tasks. Includes local persistence and a responsive design.",
-      },
-      githubLink: "https://github.com/Agus-Albarracin/uuid-store",
+  {
+    id: 2,
+    title: {
+      es: "E-commerce Platform: UUID-Store",
+      en: "Plataforma de E-commerce: UUID-Store",
     },
+    image: "https://github.com/Alejod951/Portafolio/blob/main/src/assets/FaceMesh.png?raw=true",
+    shortDescription: {
+      es: "An optimized e-commerce platform with a user-friendly interface for customers and advanced management tools for administrators.",
+      en: "Una plataforma de e-commerce optimizada con una interfaz atractiva para clientes y herramientas avanzadas de gestión para administradores.",
+    },
+    longDescription: {
+      en: "UUID-Store is a full-featured e-commerce platform built with Vite, React, Node.js, Express.js, and PostgreSQL. Customers can browse a catalog of sneakers, apply advanced filters, view trending products, and complete purchases using a payment gateway. Features include a shopping cart, user profiles, and a sleek, optimized UI. Administrators have access to detailed metrics such as sales, page visits, purchase histories, and user management, including assigning admin roles and managing stock and product details.",
+      es: "UUID-Store es una plataforma de e-commerce completa desarrollada con Vite, React, Node.js, Express.js y PostgreSQL. Los clientes pueden navegar por un catálogo de zapatillas, aplicar filtros avanzados, ver productos más solicitados y realizar compras a través de una pasarela de pago. Las funciones incluyen carrito de compras, perfiles de usuario y una interfaz optimizada y atractiva. Los administradores tienen acceso a métricas detalladas como ventas, visitas a la página, historial de compras y gestión de usuarios, incluyendo asignar roles de administrador, gestionar stock y detalles de los productos.",
+    },
+    githubLink: "https://github.com/Agus-Albarracin/uuid-store",
+  }
+  ,
   ];
